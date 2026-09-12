3dde2e601528b065d579d51d21951932a55e0d0d
vineetkpe <121609218+vineetkpe@users.noreply.github.com>
2026-09-12 12:26:26 +0530

    Mount persistent ScreenGremlin quick controls

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import OverlayControls from './OverlayControls'
 import './styles.css'
 import './desktop.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <OverlayControls />
   </React.StrictMode>,
 )
